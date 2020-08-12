50df247f3f94796354e3e16c90c7c867b1c0957b
Abhinandan-Pandey <abhinandanpandey.ap@gmail.com>
2020-08-13 02:26:27 +0530

    Updates

---
+++ precache-manifest.edd8e60ad90f0c93b4f51a6b9d0c85f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe19f5b45bc78040ca0dbb2519ed52f",
+    "url": "/web-Assignment/index.html"
+  },
+  {
+    "revision": "69716e14de533df8427e",
+    "url": "/web-Assignment/static/css/main.617ed62b.chunk.css"
+  },
+  {
+    "revision": "21790239b2599c3e3511",
+    "url": "/web-Assignment/static/js/2.5a0793a4.chunk.js"
+  },
+  {
+    "revision": "f42d151f61898dd9d5ffe0d9c5238b3c",
+    "url": "/web-Assignment/static/js/2.5a0793a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69716e14de533df8427e",
+    "url": "/web-Assignment/static/js/main.69e98b9c.chunk.js"
+  },
+  {
+    "revision": "9957dbdf946e428952bb",
+    "url": "/web-Assignment/static/js/runtime-main.a9f12096.js"
+  }
+]);
